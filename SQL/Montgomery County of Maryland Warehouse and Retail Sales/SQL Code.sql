71833e11477af63c6417f8de8e9fde44458ed1ca
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-02-11 15:53:59 +0300

    final

--- SQL/Montgomery County of Maryland Warehouse and Retail Sales/SQL Code.sql
+++ SQL/Montgomery County of Maryland Warehouse and Retail Sales/SQL Code.sql
@@ -2,3 +2,15 @@
 SELECT   MAX([RETAIL SALES])  AS_Highest_Retail_Sale
 FROM [Montgomery County of Maryland  Wine Sales].[dbo].['Warehouse_and_Retail_Sales (1)$']
 
+/****** Whats is the Average  retail sales  ******/
+SELECT   AVG([RETAIL SALES])  AS_Average_Retail_Sale
+FROM [Montgomery County of Maryland  Wine Sales].[dbo].['Warehouse_and_Retail_Sales (1)$']
+
+
+/******  Whats is the highest Warehouse sales   ******/
+SELECT MAX([WAREHOUSE SALES])  AS_Highest_Wholesale_price
+FROM [Montgomery County of Maryland  Wine Sales].[dbo].['Warehouse_and_Retail_Sales (1)$']
+
+/****** Whats is the Average Warehouse sales   ******/
+SELECT ROUND(AVG([WAREHOUSE SALES]),0)  As_Highest_warehouse_price
+FROM [Montgomery County of Maryland  Wine Sales].[dbo].['Warehouse_and_Retail_Sales (1)$']
