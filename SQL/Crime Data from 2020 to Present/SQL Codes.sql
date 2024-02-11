d68aeebf23ae8020f32449c27e8676ea995a1644
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-02-11 10:33:11 +0300

    final

--- SQL/Crime Data from 2020 to Present/SQL Codes.sql
+++ SQL/Crime Data from 2020 to Present/SQL Codes.sql
@@ -1,3 +1,7 @@
 /****** What is the higest number of rapes ******/
 SELECT MAX([Rpt Dist No]) AS Highest_number_of_rape
 FROM [City of Los Angeles  Crime Data ].[dbo].[Crime_Data_from_2020_to_Present$]
+
+/****** What is the Average number of rapes ******/
+SELECT AVG([Rpt Dist No]) AS Average_number_of_rapes
+FROM [City of Los Angeles  Crime Data ].[dbo].[Crime_Data_from_2020_to_Present$]
