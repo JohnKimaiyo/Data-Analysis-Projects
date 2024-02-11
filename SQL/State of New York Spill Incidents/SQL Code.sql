f51f8d55b0e710f041eac46ed942b531542bc876
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-02-11 14:47:03 +0300

    final

--- SQL/State of New York Spill Incidents/SQL Code.sql
+++ SQL/State of New York Spill Incidents/SQL Code.sql
@@ -1,3 +1,7 @@
-/****** Whats is the highst numer of spilage  ******/
+/****** Whats is the highst number of spilage  ******/
 SELECT  MAX([Spill Number]) AS_Highest_Number_of_Spillage
+FROM [State of New York  Spill Incidents].[dbo].['Spill_Incidents (1)$']
+
+/****** Whats is the Average number of spilage  ******/
+SELECT  ROUND(AVG([Spill Number]),0) AS_Average_Number_of_Spillage
 FROM [State of New York  Spill Incidents].[dbo].['Spill_Incidents (1)$']
