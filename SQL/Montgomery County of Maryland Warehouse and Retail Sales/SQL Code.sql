1054a736f3896d4af1432d8b1ff55d068a9d9a3a
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-02-11 15:47:33 +0300

    fimal

---
+++ SQL/Montgomery County of Maryland Warehouse and Retail Sales/SQL Code.sql
@@ -0,0 +1,4 @@
+/****** Whats is the highest retail sale  ******/
+SELECT   MAX([RETAIL SALES])  AS_Highest_Retail_Sale
+FROM [Montgomery County of Maryland  Wine Sales].[dbo].['Warehouse_and_Retail_Sales (1)$']
+
