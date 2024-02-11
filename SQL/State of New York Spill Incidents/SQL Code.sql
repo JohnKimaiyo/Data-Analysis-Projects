9f7e8489eae752aafd0f50df57fd370522cd10a4
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-02-11 11:27:24 +0300

    final

---
+++ SQL/State of New York Spill Incidents/SQL Code.sql
@@ -0,0 +1,3 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT  MAX([Spill Number]) AS_Highest_Number_of_Spillage
+FROM [State of New York  Spill Incidents].[dbo].['Spill_Incidents (1)$']
