2a41f109b69c76dbacb7e25350783507effe9552
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-02-11 15:20:07 +0300

    final

--- SQL/City of New York Summer Sports Experience/SQL Code.sql
+++ SQL/City of New York Summer Sports Experience/SQL Code.sql
@@ -11,3 +11,10 @@ SELECT TOP 5 [Park Location] , SUM([Attendance Sum]) AS_Total_Attendance
 FROM [City of New York Summer Sports Experience].[dbo].[Summer_Sports_Experience$]
 GROUP BY [Park Location]
 ORDER BY  AS_Total_Attendance DESC;
+
+
+/****** What are the top five Park Borough  locations that had the highest attendance  ******/
+SELECT TOP 5 [Borough Location] ,sum([Attendance Sum]) AS_Total_Attendance
+FROM [City of New York Summer Sports Experience].[dbo].[Summer_Sports_Experience$]
+GROUP BY [Borough Location]
+ORDER BY AS_Total_Attendance DESC;
