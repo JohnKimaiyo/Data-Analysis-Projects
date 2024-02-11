debb8713ea83a982d81beb4c0bd3ac38a641c34f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-02-11 11:27:58 +0300

    final

--- SQL/State of New York Spill Incidents/SQL Code.sql
+++ SQL/State of New York Spill Incidents/SQL Code.sql
@@ -1,3 +1,3 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
+/****** Whats is the highst numer of spilage  ******/
 SELECT  MAX([Spill Number]) AS_Highest_Number_of_Spillage
 FROM [State of New York  Spill Incidents].[dbo].['Spill_Incidents (1)$']
