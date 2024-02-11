63251124d28ce6c7e98984c2692b22a03e0a4595
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-02-11 15:17:47 +0300

    final

--- SQL/City of New York Summer Sports Experience/SQL Code.sql
+++ SQL/City of New York Summer Sports Experience/SQL Code.sql
@@ -5,3 +5,9 @@ FROM [City of New York Summer Sports Experience].[dbo].[Summer_Sports_Experience
 /******  What is the Average  attendance   ******/
 SELECT AVG([Attendance Sum])  AS_Average_Attendance
 FROM [City of New York Summer Sports Experience].[dbo].[Summer_Sports_Experience$]
+
+/****** What are the top five Park locations that had the highest attendance  ******/
+SELECT TOP 5 [Park Location] , SUM([Attendance Sum]) AS_Total_Attendance
+FROM [City of New York Summer Sports Experience].[dbo].[Summer_Sports_Experience$]
+GROUP BY [Park Location]
+ORDER BY  AS_Total_Attendance DESC;
