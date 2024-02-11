187282217caba0afe7c686aca64189d56d56e5e8
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-02-11 15:13:13 +0300

    final

--- SQL/City of New York Summer Sports Experience/SQL Code.sql
+++ SQL/City of New York Summer Sports Experience/SQL Code.sql
@@ -1,3 +1,7 @@
 /****** What is the highest attendance   ******/
 SELECT MAX([Attendance Sum])  AS_Highest_Attendance
 FROM [City of New York Summer Sports Experience].[dbo].[Summer_Sports_Experience$]
+
+/******  What is the Average  attendance   ******/
+SELECT AVG([Attendance Sum])  AS_Average_Attendance
+FROM [City of New York Summer Sports Experience].[dbo].[Summer_Sports_Experience$]
