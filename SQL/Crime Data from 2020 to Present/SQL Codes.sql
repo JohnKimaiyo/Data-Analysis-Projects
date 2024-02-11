b43ac8f1c935de3e0d283817c04c04d270549b76
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-02-11 10:41:33 +0300

    final

--- SQL/Crime Data from 2020 to Present/SQL Codes.sql
+++ SQL/Crime Data from 2020 to Present/SQL Codes.sql
@@ -6,6 +6,10 @@ FROM [City of Los Angeles  Crime Data ].[dbo].[Crime_Data_from_2020_to_Present$]
 SELECT AVG([Rpt Dist No]) AS Average_number_of_rapes
 FROM [City of Los Angeles  Crime Data ].[dbo].[Crime_Data_from_2020_to_Present$]
 
-/****** What is the Oldest Rape Victime ******/
+/****** What is the Oldest Rape Victims******/
 SELECT MAX ([Vict Age]) AS_Oldest_Rape_Victim
+FROM [City of Los Angeles  Crime Data ].[dbo].[Crime_Data_from_2020_to_Present$]
+
+/****** What is the Average Rape Victims ******/
+SELECT ROUND(AVG([Vict Age]), 0) AS Average_Rape_Victim_Age
 FROM [City of Los Angeles  Crime Data ].[dbo].[Crime_Data_from_2020_to_Present$]
