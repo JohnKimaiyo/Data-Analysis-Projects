29f5c47b59a99d6a3ee5ae432bc117425d2c6f0e
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-02-12 07:59:17 +0300

    final

--- SQL/Montgomery County of Maryland County Spending/SQL Code.sql
+++ SQL/Montgomery County of Maryland County Spending/SQL Code.sql
@@ -5,3 +5,9 @@ FROM [Montgomery County of Maryland  County Spending].[dbo].[County_Spending$]
 /******What is the Average spent  ******/
 SELECT AVG([Amount]) AS_Average_Amount_Paid
 FROM [Montgomery County of Maryland  County Spending].[dbo].[County_Spending$]
+
+/****** Whats are the Top 5 Most Expensive serveices spent on  ******/
+SELECT TOP 5 [Service],  SUM([Amount]) As_Total_Amount_Spent
+FROM [Montgomery County of Maryland  County Spending].[dbo].[County_Spending$]
+GROUP BY [Service]
+ORDER BY As_Total_Amount_Spent DESC;
