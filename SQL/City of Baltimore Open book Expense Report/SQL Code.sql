c7c3fc46e677011c0b8936dfd3bcd7f4b99f7e0e
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-02-12 07:17:23 +0300

    final

--- SQL/City of Baltimore Open book Expense Report/SQL Code.sql
+++ SQL/City of Baltimore Open book Expense Report/SQL Code.sql
@@ -5,3 +5,9 @@ FROM [City of Baltimore Open Checkbook FY2021 Dataset].[dbo].[Open_Checkbook_FY2
 /****** Whats the  average  amount paid   ******/
 SELECT ROUND(AVG([Amount]),0) AS_Average_Amount_Paid
 FROM [City of Baltimore Open Checkbook FY2021 Dataset].[dbo].[Open_Checkbook_FY2021_Dataset$]
+
+/****** Whats are the Top 5  agency that got the most funds ******/
+SELECT TOP 5[Agency] , SUM([Amount]) As_Total_Amount_Paid
+FROM [City of Baltimore Open Checkbook FY2021 Dataset].[dbo].[Open_Checkbook_FY2021_Dataset$]
+GROUP BY [Agency]
+ORDER BY As_Total_Amount_Paid DESC;
