054d5772eb1691a8f1a3c649c90b8151d23def5c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-02-12 07:23:52 +0300

    final

--- SQL/City of Baltimore Open book Expense Report/SQL Code.sql
+++ SQL/City of Baltimore Open book Expense Report/SQL Code.sql
@@ -17,3 +17,9 @@ SELECT TOP 5 [Service] ,SUM([Amount]) As_Total_Amount_Paid
 FROM [City of Baltimore Open Checkbook FY2021 Dataset].[dbo].[Open_Checkbook_FY2021_Dataset$]
 GROUP BY [Service]
 ORDER BY  As_Total_Amount_Paid DESC;
+
+/****** Whats top 5 vendors that had the highest contrcats  ******/
+SELECT TOP  5 [Vendor_Name],SUM([Amount]) AS_Total_Amount_Spent
+FROM [City of Baltimore Open Checkbook FY2021 Dataset].[dbo].[Open_Checkbook_FY2021_Dataset$]
+GROUP BY [Vendor_Name]
+ORDER BY AS_Total_Amount_Spent DESC;
