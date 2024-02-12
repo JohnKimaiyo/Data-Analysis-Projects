e91438f7021bd36344ec6316a3258a499d681368
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-02-12 08:46:22 +0300

    final

--- SQL/State of California Infectious Diseases by Disease/SQL Code.sql
+++ SQL/State of California Infectious Diseases by Disease/SQL Code.sql
@@ -1,3 +1,7 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
+/****** What is the most infected population ******/
 SELECT MAX([Population]) As_Highest_Population
+FROM [State of California Infectious Diseases].[dbo].[odp_idb_2001_2022_ddg_compliant$]
+
+/****** What is the Average  infected population ******/
+SELECT AVG([Population]) As_Average_Population
 FROM [State of California Infectious Diseases].[dbo].[odp_idb_2001_2022_ddg_compliant$]
