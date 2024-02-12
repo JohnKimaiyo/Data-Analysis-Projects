277296753be799e704cac43de8b9b359254e812c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-02-12 11:53:04 +0300

    final

---
+++ SQL/Consumer Financial Protection Bureau/SQL Code.sql
@@ -0,0 +1,3 @@
+-- Whats is the highest balance amount* /
+SELECT MAX([TOTAL OPEN ACCOUNTS AS OF END OF REPORTING YEAR]) As_Highest_Bank_Balance
+  FROM [Consumer Financial Protection Bureau College Credit Card Marketing Agreements Data].[dbo].['cfpb_college-credit-card-agreem$']
