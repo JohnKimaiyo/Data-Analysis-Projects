84d063115659f315aeaa1c564f153eda50c500f9
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-02-12 07:50:27 +0300

    final

--- SQL/Montgomery County of Maryland County Spending/SQL Code.sql
+++ SQL/Montgomery County of Maryland County Spending/SQL Code.sql
@@ -1,3 +1,7 @@
 /****** What is the highest spent  ******/
 SELECT MAX([Amount]) AS_Total_Amount_Paid
 FROM [Montgomery County of Maryland  County Spending].[dbo].[County_Spending$]
+
+/******What is the Average spent  ******/
+SELECT AVG([Amount]) AS_Average_Amount_Paid
+FROM [Montgomery County of Maryland  County Spending].[dbo].[County_Spending$]
