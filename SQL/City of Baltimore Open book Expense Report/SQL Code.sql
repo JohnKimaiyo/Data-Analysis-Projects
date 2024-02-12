f23aa6f7ecedf442f8e08fe95fcd5cf123dec0bd
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-02-12 07:13:24 +0300

    final

--- SQL/City of Baltimore Open book Expense Report/SQL Code.sql
+++ SQL/City of Baltimore Open book Expense Report/SQL Code.sql
@@ -1,3 +1,7 @@
 /****** Whats the most amount paid  ******/
 SELECT MAX([Amount]) AS_Most_Amount_Paid
 FROM [City of Baltimore Open Checkbook FY2021 Dataset].[dbo].[Open_Checkbook_FY2021_Dataset$]
+
+/****** Whats the  average  amount paid   ******/
+SELECT ROUND(AVG([Amount]),0) AS_Average_Amount_Paid
+FROM [City of Baltimore Open Checkbook FY2021 Dataset].[dbo].[Open_Checkbook_FY2021_Dataset$]
